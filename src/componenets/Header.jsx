da3c663e8b723cc909e8aebdcef0efa9566ed105
moonjunsain <moonjunsain@gmail.com>
2023-08-30 19:45:25 -0700

    added header componenet

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -5,10 +5,38 @@ export default function Header(){
 
     return(
         <header>
+            <h1>
+                <Link
+                to='/'>
+                    June Moon
+                </Link>
+            </h1>
             <nav className="nav">
-                <li className="nav-link active" aria-current="page" href="#">Active</li>
-                <li className="nav-link" href="#">Link</li>
-                <li className="nav-link" href="#">Link</li>
+                <li className="nav-item">
+                    <Link to='/' className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/About' className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}>
+                        About Me
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/Portfolio' className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>
+                        Portfolio
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/Resume' className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>
+                        Resume
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/Contact' className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>
+                        Contact
+                    </Link>
+                </li>
             </nav>
         </header>       
     )
